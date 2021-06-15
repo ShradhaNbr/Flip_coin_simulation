c373059f374f6813e192022c18e272e292c67a72
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 21:01:47 +0530

    Refactored loop to print heads and tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
+declare -A coin
+read -p "Enter key value" key
+for((i=0;i<$key;i++))
+do
 random=$((RANDOM%2))
-echo $random
+coin=$random
+if [ $random -eq 0 ]
+then
+echo "Heads"
+else
+echo "Tails"
+fi
+done
