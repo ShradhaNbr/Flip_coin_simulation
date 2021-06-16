7c9005bf60c38df3f0caa3b001ef71a5fd0a4082
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-16 08:23:45 +0530

    Refactored the code to calculate percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,14 +1,21 @@
 #!/bin/bash -x
 declare -A coin
+heads=0
+tails=0
 read -p "Enter key value" key
 for((i=0;i<$key;i++))
 do
-random=$((RANDOM%2))
-coin=$random
-if [ $random -eq 0 ]
+coin[random]="$((RANDOM%2))"
+if [ ${coin[@]} -eq 0 ]
 then
-echo "Heads"
+heads=$(($heads+1))
+echo "Number of heads $heads"
+per_heads=`echo - | awk '{print ('$heads' / '$key') * '100' }'`
+echo "Percentage of heads $per_heads"
 else
-echo "Tails"
+tails=$(($tails+1))
+echo "number of tails $tails"
+per_tails=`echo - | awk '{print ('$tails' / '$key') * '100' }'`
+echo "Percentage of tails $per_tails"
 fi
 done
